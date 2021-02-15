d8ab682d8a44ba7194361b69a6473bbe1c13f0f4
Kat Bohringer <anyretak@users.noreply.github.com>
2021-02-15 08:38:44 +0100

    Cleaned up the code
    -Removed repeating/unnecessary code;
    -Fixed a bug where on draw game wouldn't reset

--- scripts/tictactoe.js
+++ scripts/tictactoe.js
@@ -1,28 +1,22 @@
 "use strict";
-//initialize array of the game board, for easier reset later
+
+//initialize array of the game board
 //decide on game board size (here it is a 3x3 board)
-//initialize the first player (X) and player scores
+//set up player colours
 const board = [];
 const size = 3;
+const colorClass = ["draw", "player1", "player2"]
+
+//initialize the first player (X) and player scores
 let player = 1;
 let playerWin1 = 0;
 let playerWin2 = 0;
 
-//set up the score keeping areas and info message (the info message colour will change based on the player)
+//create variables for score keeping, info messages and reset button 
 let scoreLine1 = document.getElementById("scorePlayer1");
-scoreLine1.style.color = "limegreen";
 let scoreLine2 = document.getElementById("scorePlayer2");
-scoreLine2.style.color = "yellow";
 let infoLine = document.getElementById("gameinfo");
-infoLine.style.color = "limegreen";
-infoLine.innerText = "Player " + player + ", it's your turn to play!";
-
-//set up the reset button
-let resetButton = document.createElement("button");
-resetButton.innerText = "Play again?"
-resetButton.style.display = "none";
-document.getElementById("resetButton").appendChild(resetButton);
-resetButton.addEventListener("click", resetGame);
+let resetButton = document.getElementById("resetButton");
 
 //GAMEBOARD functionality
 function drawTable() {
@@ -47,47 +41,51 @@ function drawTable() {
     }
     //add the table to the page
     document.getElementById("gameboard").appendChild(table);
+
+    //set starting variables
+    infoLine.className = colorClass[1];
+    infoLine.textContent = "Player " + player + ", it's your turn to play!";
+    resetButton.addEventListener("click", resetGame);
 }
 
 //PLAY functionality
 function playCell() {
-
     //check if play is possible
-    if (this.innerHTML === "") {
+    if (this.textContent === "") {
         //play based on the current player (game always starts with play 1, which is X)
         switch (player) {
             case 1:
-                this.innerText = "X";
+                this.textContent = "X";
                 //check if the user has won on their move
                 //update the message and the win score
                 //reset the game (button)
                 if (checkWin()) {
-                    infoLine.style.color = "limegreen";
+                    infoLine.className = colorClass[1];
                     playerWin1++;
-                    infoLine.innerText = "Player " + player + " has won the game!";
-                    scoreLine1.innerText = "Player 1 Score: " + playerWin1;
-                    resetButton.style.display = "block";
+                    infoLine.textContent = "Player " + player + " has won the game!";
+                    scoreLine1.textContent = playerWin1;
+                    resetButton.classList.toggle("buttonDisplay");
                     break;
                     //otherwise switch to player 2
                 } else {
                     player = 2;
-                    infoLine.style.color = "yellow";
-                    infoLine.innerText = "Player " + player + ", it's your turn to play!";
+                    infoLine.className = colorClass[2];
+                    infoLine.textContent = "Player " + player + ", it's your turn to play!";
                     break;
                 }
             case 2:
-                this.innerText = "O";
+                this.textContent = "O";
                 if (checkWin()) {
-                    infoLine.style.color = "yellow";
+                    infoLine.className = colorClass[2];
                     playerWin2++;
-                    infoLine.innerText = "Player " + player + " has won the game!";
-                    scoreLine2.innerText = "Player 2 Score: " + playerWin2;
-                    resetButton.style.display = "block";
+                    infoLine.textContent = "Player " + player + " has won the game!";
+                    scoreLine2.textContent = playerWin2;
+                    resetButton.classList.toggle("buttonDisplay");
                     break;
                 } else {
                     player = 1;
-                    infoLine.style.color = "limegreen";
-                    infoLine.innerText = "Player " + player + ", it's your turn to play!";
+                    infoLine.className = colorClass[1];
+                    infoLine.textContent = "Player " + player + ", it's your turn to play!";
                     break;
                 }
         }
@@ -98,20 +96,22 @@ function playCell() {
 
     //check if any empty spaces remain on the board
     //if no empty spaces remain and no user won declare a draw and reset the game
-    if (board.every(cell => cell.innerHTML !== "")) {
-        infoLine.style.color = "white";
-        infoLine.innerText = "Game ended in a draw";
-        resetButton.style.display = "block";
-    };
+    if (board.every(cell => cell.textContent !== "")) {
+        if (!checkWin()) {
+            infoLine.className = colorClass[0];
+            infoLine.textContent = "Game ended in a draw";
+            resetButton.classList.toggle("buttonDisplay");
+        }
+    }
 }
 
 //CHECKING WIN CONDITIONS
 //check win by row
 function checkWinRow() {
-    for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < size; i++) {
         const winArray = [];
-        for (let j = 0; j < 3; j++) {
-            winArray.push(document.getElementById("tableRow" + i).cells[j].innerText);
+        for (let j = 0; j < size; j++) {
+            winArray.push(document.getElementById("tableRow" + i).cells[j].textContent);
         }
         if (winArray.every(field => field == "X") || winArray.every(field => field == "O")) {
             return true;
@@ -122,10 +122,10 @@ function checkWinRow() {
 
 //check win by column
 function checkWinCol() {
-    for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < size; i++) {
         const winArray = [];
-        for (let j = 0; j < 3; j++) {
-            winArray.push(document.getElementById("tableRow" + j).cells[i].innerText);
+        for (let j = 0; j < size; j++) {
+            winArray.push(document.getElementById("tableRow" + j).cells[i].textContent);
         }
         if (winArray.every(field => field == "X") || winArray.every(field => field == "O")) {
             return true;
@@ -137,8 +137,8 @@ function checkWinCol() {
 //check win by diagonal (1)
 function checkWinDiag1() {
     const winArray = [];
-    for (let i = 0; i < 3; i++) {
-        winArray.push(document.getElementById("tableRow" + i).cells[i].innerText);
+    for (let i = 0; i < size; i++) {
+        winArray.push(document.getElementById("tableRow" + i).cells[i].textContent);
     }
     if (winArray.every(field => field == "X") || winArray.every(field => field == "O")) {
         return true;
@@ -149,8 +149,8 @@ function checkWinDiag1() {
 //check win by diagonal (2)
 function checkWinDiag2() {
     const winArray = [];
-    for (let i = 0; i < 3; i++) {
-        winArray.push(document.getElementById("tableRow" + i).cells[3 - i - 1].innerText);
+    for (let i = 0; i < size; i++) {
+        winArray.push(document.getElementById("tableRow" + i).cells[3 - i - 1].textContent);
     }
     if (winArray.every(field => field == "X") || winArray.every(field => field == "O")) {
         return true;
@@ -169,8 +169,8 @@ function checkWin() {
 //reset the game
 function resetGame() {
     player = 1;
-    board.forEach(cell => cell.innerHTML = "");
-    resetButton.style.display = "none";
+    board.forEach(cell => cell.textContent = "");
+    resetButton.classList.toggle("buttonDisplay");
 }
 
 //initialize game board
